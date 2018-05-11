99d6849c9b1f77adc311ba6fa5d7c93f56c5ded0
Chris Allen <cma@bitemyapp.com>
2018-05-10 19:04:35 -0500

    de-nixification

--- shell.nix
+++
@@ -1,7 +0,0 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? "default"}:
-let
-  inherit (nixpkgs) pkgs;
-  drv = import ./default.nix { inherit nixpkgs compiler; };
-  drvWithTools = pkgs.haskell.lib.addBuildDepends drv [ pkgs.cabal-install ];
-in
-  if pkgs.lib.inNixShell then drvWithTools.env else drvWithTools
